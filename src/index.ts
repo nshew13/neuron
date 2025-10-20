28e4439666a7bd59dee3b1a06cc11c55bafc9252
nshew13 <328432+nshew13@users.noreply.github.com>
2025-10-19 20:21:19 -0400

    feat: adds columns

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,22 @@ import {Neuron} from './Neuron.js';
 		}
 	);
 
+	const FORMAT_PERCENT = new Intl.NumberFormat(
+		'en-US',
+		{
+			style: 'percent',
+			minimumSignificantDigits: 3,
+			maximumSignificantDigits: 5,
+		}
+	);
+
+	const formatterPercent: ValueFormatterFunc = (params: ValueFormatterParams) => {
+		if (params.value === 0) {
+			return '-';
+		}
+
+		return FORMAT_PERCENT.format(params.value / 100);
+	}
 	const formatterRate: ValueFormatterFunc = (params: ValueFormatterParams) => FORMAT_CURRENCY.format(params.value);
 
 	const dialogDetails = new DialogDetails('sl-dialog[data-details]');
@@ -28,9 +44,12 @@ import {Neuron} from './Neuron.js';
 
 		// reusable, custom column types
 		const columnTypes: GridOptions['columnTypes'] = {
+			percent: {
+				valueFormatter: formatterPercent,
+			},
 			rate: {
 				valueFormatter: formatterRate,
-			}
+			},
 		};
 
 		const gridOptions: GridOptions = {
@@ -43,6 +62,15 @@ import {Neuron} from './Neuron.js';
 			paginationPageSizeSelector: [25, 50, 100, 250],
 			rowData: [],
 
+			/*
+			 * "constant" fields in this data:
+			 *  - code
+			 *  - code_type
+			 *  - negotiation_type
+			 *  - network_name
+			 *  - payer_name
+			 */
+
 			columnDefs: [
 				{
 					headerName: 'Entity Name',
@@ -50,6 +78,19 @@ import {Neuron} from './Neuron.js';
 					cellClass: 'clickable',
 					onCellClicked: (e) => dialogDetails.show(e),
 				},
+				// {
+				// 	field: 'mrf_rate',
+				// 	type: 'rate',
+				// 	cellClass: params => {
+				// 		let classes = 'right-align';
+				//
+				// 		if (params.value !== (params.data as IRateRecord).rate) {
+				// 			classes += ' warning';
+				// 		}
+				//
+				// 		return classes;
+				// 	}
+				// },
 				{
 					headerName: 'Rate',
 					field: 'rate',
@@ -57,12 +98,22 @@ import {Neuron} from './Neuron.js';
 					cellClass: 'right-align',
 				},
 				{
-					headerName: 'Payer Name',
-					field: 'payer_name',
+					headerName: '% Baseline',
+					field: 'relative_to_baseline',
+					type: 'percent',
+					cellClass: 'right-align',
 				},
 				{
-					headerName: 'Region',
-					field: 'network_region',
+					headerName: 'Taxonomy',
+					field: 'npi_taxonomy_name',
+				},
+				{
+					headerName: 'NPI Region',
+					field: 'npi_region',
+				},
+				{
+					headerName: 'Billing Class',
+					field: 'billing_class',
 				},
 			],
 		};
@@ -72,7 +123,7 @@ import {Neuron} from './Neuron.js';
 			const gridApi = createGrid(myGridElement as HTMLElement, gridOptions as any);
 			new Neuron(gridApi);
 
-			// initially sort by ID then rate
+			// initially sort by name then rate
 			gridApi.applyColumnState({
 				state: [
 					{ colId: 'entity_name', sort: 'asc', sortIndex: 0 },
